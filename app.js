5a82958129c65418ef18efe9423962823ec56f56
TarasKing <ivanovtaras@live.ru>
2023-02-10 22:11:25 -0800

    added the rest controllers / frontend

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+
 require("dotenv").config();
 
 // middleware
-
+app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task manager app!");
-});
 
 app.use("/api/v1/tasks", tasks);
 
--- models/task.js
+++ models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  title: String,
-  completed: Boolean,
+  title: {
+    type: String,
+    required: [true, "Please add a title for your task"],
+    trim: true,
+    maxlength: [30, "Title cannot be more than 30 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);

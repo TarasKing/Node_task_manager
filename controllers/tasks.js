774c58017a0e1cbb413cd6345a26bf56e91f0b63
TarasKing <ivanovtaras@live.ru>
2023-02-11 10:23:02 -0800

    refactoring code / added error handler

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,66 +1,51 @@
 const Task = require("../models/Task");
+const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks: tasks });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks: tasks });
+});
+
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    const error = new Error(`No task with id: ${taskID}`);
+    error.status = 404;
+    return next(error);
+    return res.status(404).json({ msg: `No task with id: ${taskID}` });
   }
-};
+  res.status(200).json({ task });
+});
 
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id: ${taskID}` });
   }
-};
+  res.status(200).json({ task });
+});
 
-const getTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id: ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id: ${taskID}` });
   }
-};
 
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id: ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id: ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+  res.status(200).json({ task });
+});
 
 module.exports = {
   getAllTasks,
@@ -69,3 +54,19 @@ module.exports = {
   updateTask,
   deleteTask,
 };
+
+// before refactoring the code getAllTasks looked like this:
+
+// const getAllTasks = async (req, res) => {
+//   try {
+//     const tasks = await Task.find({});
+//     res.status(200).json({ tasks: tasks });
+//     //other possible ways to send the response:
+//     // res.status(200).json({ tasks, nbHits: tasks.length });
+//     // res.status(200).json({ status: "success", data:{tasks, nbHits: tasks.length} });
+//   } catch (error) {
+//     res.status(500).json({ msg: error });
+//   }
+// };
+
+// the rest controllers also was refactoring in the same way
---
+++ errors/custom-error.js
@@ -0,0 +1,12 @@
+class CustomAPIEror extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+const createCustomError = (msg, statusCode) => {
+  return new CustomAPIEror(msg, statusCode);
+};
+
+module.exports = { createCustomError, CustomAPIEror };
